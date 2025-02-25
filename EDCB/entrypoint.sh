412172cd3ac95e848631d955f40aa015ed31ab25
nunawa <71294849+nunawa@users.noreply.github.com>
2025-02-26 01:10:37 +0900

    チャンネルスキャンを別サービスにし、/var/local/edcb以下はVolumeで共有する改善

--- EDCB/entrypoint.sh
+++ EDCB/entrypoint.sh
@@ -2,5 +2,6 @@
 
 sleep 15
 
-/usr/local/bin/EpgDataCap_Bon -d BonDriver_LinuxMirakc.so -chscan \
-&& /usr/local/bin/EpgTimerSrv
+/usr/local/bin/EpgDataCap_Bon -d BonDriver_LinuxMirakc.so -chscan
+
+echo "Exiting..."
